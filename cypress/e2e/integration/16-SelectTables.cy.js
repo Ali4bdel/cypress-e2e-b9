b25f326902e040f44ba69d88087562ea1c16865d
Ali4bdel <ali4bdel@gmail.com>
2024-05-25 12:29:02 -0500

    cloned repo

---
+++ cypress/e2e/integration/16-SelectTables.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Static Tables', () => {
+
+
+    /**
+     * Test Case
+     * Scenario: Verify the headers of the table
+     * 
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Click on the "Tables" card
+     * 3. On the static table, Validate the headers of the table are "Rank", "Company", "Employees", "Country"
+     * 
+     * 
+     * NOTE: USE POM, and Fixtures
+     */
+    it('Verify the headers of the table', () => {
+  
+    })
+  })
